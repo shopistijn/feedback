72dcc56c4b256c7de993ed1372669d150b541496
shopistijn <stijn@angelodorny.be>
2025-05-30 23:09:23 +0200

    Update reviews.js

--- api/reviews.js
+++ api/reviews.js
@@ -1,1 +1,40 @@
+const axios = require('axios');
 
+const FIREBASE_URL = 'https://trustprofilereviews-default-rtdb.firebaseio.com/reviews';
+
+function safeEmail(email) {
+  return email.replace(/@/g, '_at_').replace(/\./g, '_dot_');
+}
+
+export default async function handler(req, res) {
+  const {
+    query: { email },
+    method,
+  } = req;
+
+  if (method !== 'GET') {
+    res.setHeader('Allow', ['GET']);
+    return res.status(405).end(`Method ${method} Not Allowed`);
+  }
+
+  if (!email) {
+    return res.status(400).json({ error: 'Email is required' });
+  }
+
+  try {
+    const safe_email = safeEmail(email);
+    const response = await axios.get(`${FIREBASE_URL}/${safe_email}.json`);
+    const data = response.data;
+
+    if (!data) {
+      return res.json({ data: [] });
+    }
+
+    const reviews = Object.values(data);
+
+    res.status(200).json({ data: reviews });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: 'Failed to fetch reviews' });
+  }
+}
